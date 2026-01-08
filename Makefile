2955b11c328e51fcaf9a0a045819a248fd2e30c7
Victor Cortes Figueroa <vmcortesf@gmail.com>
2026-01-07 22:54:53 -0600

    Adding calc sample

---
+++ Makefile
@@ -0,0 +1,5 @@
+.PHONY: run_calc
+
+
+run_calc:
+	uv run sample/calculator/calc.py
---
+++ sample/calculator/calc.py
@@ -0,0 +1,33 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Calc App"
+    result = ft.Text(value="0")
+
+    page.add(
+        result,
+        ft.Button("AC"),
+        ft.Button("+/-"),
+        ft.Button("%"),
+        ft.Button("/"),
+        ft.Button("7"),
+        ft.Button("8"),
+        ft.Button("9"),
+        ft.Button("*"),
+        ft.Button("4"),
+        ft.Button("5"),
+        ft.Button("6"),
+        ft.Button("-"),
+        ft.Button("1"),
+        ft.Button("2"),
+        ft.Button("3"),
+        ft.Button("+"),
+        ft.Button("0"),
+        ft.Button("."),
+        ft.Button("="),
+    )
+
+
+if __name__ == "__main__":
+    ft.run(main)
