009cdc17171e67d395d4fe680e3a4a072111cd65
Victor Cortes Figueroa <vmcortesf@gmail.com>
2026-01-07 22:55:32 -0600

    Arrange calc buttons

--- sample/calculator/calc.py
+++ sample/calculator/calc.py
@@ -6,26 +6,46 @@ def main(page: ft.Page):
     result = ft.Text(value="0")
 
     page.add(
-        result,
-        ft.Button("AC"),
-        ft.Button("+/-"),
-        ft.Button("%"),
-        ft.Button("/"),
-        ft.Button("7"),
-        ft.Button("8"),
-        ft.Button("9"),
-        ft.Button("*"),
-        ft.Button("4"),
-        ft.Button("5"),
-        ft.Button("6"),
-        ft.Button("-"),
-        ft.Button("1"),
-        ft.Button("2"),
-        ft.Button("3"),
-        ft.Button("+"),
-        ft.Button("0"),
-        ft.Button("."),
-        ft.Button("="),
+        ft.Row(controls=[result]),
+        ft.Row(
+            controls=[
+                ft.Button("AC"),
+                ft.Button("+/-"),
+                ft.Button("%"),
+                ft.Button("/"),
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.Button("7"),
+                ft.Button("8"),
+                ft.Button("9"),
+                ft.Button("*"),
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.Button("4"),
+                ft.Button("5"),
+                ft.Button("6"),
+                ft.Button("-"),
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.Button("1"),
+                ft.Button("2"),
+                ft.Button("3"),
+                ft.Button("+"),
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.Button("0"),
+                ft.Button("."),
+                ft.Button("="),
+            ]
+        ),
     )
 
 
